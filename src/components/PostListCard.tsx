4372de8f8e31f28957192514cb0d943b2a66ce2d
25rae25 <whdudfo0124@gmail.com>
2024-03-21 00:28:15 +0900

    react-portal 추가

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { SimplePost } from "@/model/post"
 import Avatar from "./Avatar";
 import Image from "next/image";
-
 import CommentForm from "./CommentForm";
 import ActionBar from "./ActionBar";
+import { useState } from "react";
+import ModalPortal from "./ui/ModalPortal";
+import PostModal from "./PostModal";
 
 type Props = {
 	post: SimplePost;
@@ -12,6 +16,7 @@ type Props = {
 
 export default function PostListCard({post, priority = false} : Props) {
 	const {userImage, username, image, createdAt, likes, text} = post;
+	const [openModal, setOpenModal] = useState(false);
 
 	return(
 		<article className="rounded-lg shadow-md border border-gray-200">
@@ -26,9 +31,17 @@ export default function PostListCard({post, priority = false} : Props) {
 				width={500} 
 				height={500}
 				priority={priority} 
+				onClick={() => setOpenModal(true)}
 			/>
 			<ActionBar likes={likes} username={username} text={text} createdAt={createdAt}  />
 			<CommentForm />
+			{openModal && (
+				<ModalPortal>
+					<PostModal onClose={() => setOpenModal(false)}>
+						<p>포스트 상세 페이지!!!</p>
+					</PostModal>		
+				</ModalPortal>
+			)}
 		</article>
 	)
 }
