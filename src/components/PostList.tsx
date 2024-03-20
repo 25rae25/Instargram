c456a344334f271289b127d6b0ab3c238650e444
25rae25 <whdudfo0124@gmail.com>
2024-03-20 23:59:39 +0900

    post 리팩토링 + Gridspinner 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,6 +4,7 @@ import { SimplePost } from "@/model/post";
 import { GridLoader } from "react-spinners";
 import useSWR from "swr"
 import PostListCard from "./PostListCard";
+import GridSpineer from "./ui/icons/GridSpinner";
 
 export default function PostList() {
 	const {data: posts, isLoading: loading} = useSWR<SimplePost[]>('/api/posts');
@@ -13,14 +14,14 @@ export default function PostList() {
 		<section>
 			{loading && (
 				<div className="text-center mt-32">
-					<GridLoader color="red" />
+					<GridSpineer color="red" />
 				</div>
 			)}
 			{ posts && 
 				<ul>
-					{posts.map((post) => (
+					{posts.map((post, index) => (
 						<li key={post.id} className="mb-4">
-							<PostListCard post={post} />
+							<PostListCard post={post} priority={index < 2} />
 						</li>
 					))}
 				</ul>
