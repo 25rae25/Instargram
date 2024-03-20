f2dfac39120be61af1733a72503c3bef6c441160
25rae25 <whdudfo0124@gmail.com>
2024-03-20 15:31:09 +0900

    spiner 라이브러리 + followingbar 수정

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -10,7 +10,7 @@ export default function Avatar({ image, size='normal', highlight = false }: Prop
 		<div className={getContainerStyle(size, highlight)}>
 			{/* eslint-disable-next-line @next/next/no-img-element */}
 			<img 
-				className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(size)}`} 
+				className={`bg-white object-cover rounded-full p-[0.1rem] ${getImageSizeStyle(size)}`} 
 				alt="user profile" 
 				src={image ?? undefined} 
 				referrerPolicy="no-referrer"
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,11 +1,31 @@
 'use client'
 
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
 import useSWR from "swr"
+import Avatar from "./Avatar";
 
 export default function FollowingBar() {
-	const {data, isLoading, error} = useSWR('/api/me');
+	const {data, isLoading: loading, error} = useSWR<DetailUser>('/api/me');
+	const users = data?.following;
 
 	return(
-		<p>FollowingBar</p>
+		<section>
+			{loading ? <PropagateLoader size={8} color="red" /> :
+			(!users || users.length === 0) && <p>{`You don't have following`}</p>}
+			{
+				users && users.length > 0 && <ul>
+					{users.map(({image, username}) => (
+						<li key={username}>
+							<Link href={`/user/${username}`}>
+								<Avatar image={image} highlight />
+								<p>{username}</p>
+							</Link>
+						</li>	
+					))}
+				</ul>
+			}
+		</section>
 	)
 }
