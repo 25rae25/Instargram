c409a4f3fc7585c63cacd97bb682caaff70925dd
25rae25 <whdudfo0124@gmail.com>
2024-03-21 15:45:43 +0900

    search component 추가

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,16 +1,39 @@
 'use client'
 
-import { useState } from "react"
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react"
 import useSWR from "swr";
+import GridSpineer from "./ui/icons/GridSpinner";
 
 export default function UserSearch() {
 	const [keyword, setKeyword] = useState('');
-	const {data, isLoading, error} = useSWR(`/api/search/${keyword}`);
-	console.log(data, '11111');
+	const {data: users, isLoading, error} = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+
+	const onSubmit = (e: FormEvent) => {
+		e.preventDefault();
+	}
 
 	return(
 		<>
-
+			<form onSubmit={onSubmit}>
+				<input type="text"
+				 autoFocus 
+				 placeholder="Search for a username or name" 
+				 value={keyword} 
+				 onChange={(e) => setKeyword(e.target.value)} />
+			</form>
+			{error && <p>무언가가 잘못 되었음</p>}
+			{isLoading && <GridSpineer />}
+			{!isLoading && !error && users?.length === 0 && (
+				<p>찾는 사용자가 없음</p>
+			)}
+			<ul>
+				{users && users.map((user) => (
+					<li key={user.username}>
+						<p>{user.username}</p>
+					</li>
+				))}
+			</ul>
 		</>
 	)
 }
