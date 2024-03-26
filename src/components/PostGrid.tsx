3fc4a1ec563b76f42432551071546c1acfa55598
25rae25 <whdudfo0124@gmail.com>
2024-03-26 14:56:46 +0900

    cache context로 감싸기

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -1,30 +1,23 @@
-import { SimplePost } from '@/model/post';
-import useSWR from 'swr';
+import usePosts from '@/hooks/posts';
 import PostGridCard from './PostGridCard';
 import GridSpinner from './ui/GridSpinner';
 
-type Props = {
-  username: string;
-  query: string;
-};
-export default function PostGrid({ username, query }: Props) {
-  const {
-    data: posts,
-    isLoading,
-    error,
-  } = useSWR<SimplePost[]>(`/api/users/${username}/${query}`);
 
-  return (
-    <div className='w-full text-center'>
-      {isLoading && <GridSpinner />}
-      <ul className='grid grid-cols-3 gap-4 py-4 px-8'>
-        {posts &&
-          posts.map((post, index) => (
-            <li key={post.id}>
-              <PostGridCard post={post} priority={index < 6} />
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
+
+export default function PostGrid() {
+	const {posts, isLoading} = usePosts();
+	
+	return (
+		<div className='w-full text-center'>
+			{isLoading && <GridSpinner />}
+			<ul className='grid grid-cols-3 gap-4 py-4 px-8'>
+				{posts &&
+					posts.map((post, index) => (
+						<li key={post.id}>
+							<PostGridCard post={post} priority={index < 6} />
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 }
